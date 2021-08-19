72a9b9f14c2bfe69bd3d1e73594c8c70f53c2748
gmlrudpark <phk08060@gmail.com>
2021-08-19 23:54:48 +0900

    2nd todo list

--- js/todo.js
+++ js/todo.js
@@ -15,12 +15,15 @@ function deleteToDo(event) {
     // console.dir(event.target);
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter(todo => todo.id !== parseInt(li.id) );
+    saveToDos();
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "X";
     button.addEventListener("click", deleteToDo)
@@ -36,8 +39,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = ""
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text : newTodo,
+        id : Date.now()
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 

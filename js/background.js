6fd8d7b520e9cfa331e0efd70288164d1aac58bb
gmlrudpark <phk08060@gmail.com>
2021-09-05 23:51:03 +0900

    nomad last assignment

--- js/background.js
+++ js/background.js
@@ -4,8 +4,10 @@ const images = [
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
-const bgImage = document.createElement("img");
+// const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+// bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
+
+document.body.style.backgroundImage = `url(img/${chosenImage})`;
--- js/greeting.js
+++ js/greeting.js
@@ -2,6 +2,7 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const infoWrap = document.querySelector("#info-wrap");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -16,7 +17,8 @@ function onLoginSubmit(event) {
 
 function paintGreeting(username) {
     greeting.innerText = `Hello ${username}`
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    // greeting.classList.remove(HIDDEN_CLASSNAME);
+    infoWrap.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUserName = localStorage.getItem(USERNAME_KEY)
